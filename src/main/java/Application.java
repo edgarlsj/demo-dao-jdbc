869d771b7e10fc2df17dd729a647db23c84c964e
Edgar Junior <edgarlsj@gmail.com>
2024-09-25 10:01:37 -0400

    Department entites class

---
+++ src/main/java/Application.java
@@ -0,0 +1,10 @@
+import entities.Department;
+
+public class Application {
+    public static void main(String[] args) {
+
+        Department obj = new Department(2,"books");
+        System.out.println(obj);
+
+    }
+}
