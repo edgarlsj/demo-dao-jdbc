622c2c33699c51498fa75651584df696946b3eb7
Edgar Junior <edgarlsj@gmail.com>
2024-10-01 14:14:41 -0400

    add seller

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,16 @@
 import entities.Department;
+import entities.Seller;
+
+import java.util.Date;
 
 public class Application {
     public static void main(String[] args) {
 
         Department obj = new Department(2,"books");
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date() ,3000.00,obj);
         System.out.println(obj);
+        System.out.println(seller);
 
     }
 }
